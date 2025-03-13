0d7769ff2d56e3406fe84281305dcb9237aa3983
zaijan <jgodito27@gmail.com>
2025-03-14 00:46:50 +0800

    add username logo initials

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,46 @@
-import { UserButton, useUser } from "@clerk/clerk-react"
+import { useEffect, useState } from "react";
+import { removeToken } from "../utils/authToken";
+import { getUserData } from "../services/api";
 
 export default function Navbar() {
-  const { isSignedIn, user } = useUser()
-  if (!isSignedIn) return null
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getUserData();
+      if (userData && userData.username) {
+        setUser(userData);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (!user) return null;
+
+  const avatarUrl = `https://api.dicebear.com/9.x/initials/svg?seed=${user?.username}`;
 
   return (
     <header className="flex justify-between p-4 bg-gray-200 w-full m-auto max-w-6xl">
-        <nav className="flex max-w-6xl justify-between p-4 bg-gray-200 w-full">
-        <h1>BOSS, {user?.firstName || "User"}! 👋</h1>
-        <div>
-          <UserButton />
+      <nav className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-3">
+          <img
+            src={avatarUrl}
+            alt="avatar"
+            className="w-10 h-10 rounded-full"
+          />
+          <h1>Welcome, {user?.username} 👋</h1>
         </div>
-        </nav>
+        <button
+          onClick={() => {
+            removeToken();
+            window.location.reload();
+          }}
+          className="bg-red-500 text-white px-3 py-1 rounded"
+        >
+          Sign Out
+        </button>
+      </nav>
     </header>
-  )
+  );
 }
-
